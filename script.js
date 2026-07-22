eaec541e077864f4921c5f385298d122d5f49ec3
psklvs-crypto <psklvs@gmail.com>
2026-07-22 20:42:00 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+const btn = document.getElementById("startBtn");
+
+btn.addEventListener("click",()=>{
+
+document.body.style.transition="2s";
+
+document.body.style.background="#000";
+
+setTimeout(()=>{
+
+alert("✨ Welcome Hemadarshan ✨\n\nYour magical birthday journey begins now...");
+
+},800);
+
+});
